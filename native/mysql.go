c51dee713e9c1e324ed5074a620058b5852fb9c3
Daniel Heath <daniel.r.heath@gmail.com>
2013-08-12 16:28:38 +1000

    Specify that error comes from mymysql

--- native/mysql.go
+++ native/mysql.go
@@ -131,7 +131,7 @@ func (my *Conn) NetConn() net.Conn {
 
 type timeoutError struct{}
 
-func (e *timeoutError) Error() string   { return "i/o timeout" }
+func (e *timeoutError) Error() string   { return "i/o timeout (mysql went away)" }
 func (e *timeoutError) Timeout() bool   { return true }
 func (e *timeoutError) Temporary() bool { return true }
 
